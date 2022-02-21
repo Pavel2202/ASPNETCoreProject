9d8d9c3e7091dd8be582015667836fb55dbaa10e
Pavel2202 <paveltimenov22@gmail.com>
2022-02-21 08:59:04 +0200

    Initial create

---
+++ FitnessSite/FitnessSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace FitnessSite.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
